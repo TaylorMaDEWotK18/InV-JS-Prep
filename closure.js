0a9db5760cc51599d4cf6250438ded7c16006ca2
TaylorMade <warriorotk18@gmail.com>
2022-10-15 15:13:03 -0600

    made sub and multiply functions

--- closure.js
+++ closure.js
@@ -10,9 +10,35 @@ function sonLove() {
 }
 sonLove();
 
+//I wanted to create an example of how closure will and won't work
 function innScp() {
 	let exNum = 18;
 	console.log(exNum);
 }
 innScp();
-console.log(exNum);
+// console.log(exNum);
+
+//Lets have some fun with closures with some math 
+function makeAdder(x) {
+	return function(y) {
+		return x + y;
+	}
+}
+const add3 = makeAdder(3);
+console.log(add3(10));
+
+function makeSubby(x) {
+	return function(y) {
+		return x - y;
+	}
+}
+const sub7 = makeSubby(7);
+console.log(sub7(5));
+
+function makeMulti(x) {
+	return function(y) {
+		return x * y;
+	}
+}
+const mul12 = makeMulti(12);
+console.log(mul12(6));
