b3a22b2c5c21990ec583c589f7bd6e91cb323fe3
TaylorMade <warriorotk18@gmail.com>
2022-10-18 16:19:05 -0600

    created for loop and displayed higher order function powaa!!

--- js/function.js
+++ js/function.js
@@ -10,10 +10,26 @@ function toCelsius(fahrenheit) {
 }
 let temp = `It's currently is ${toCelsius(86)} degrees outside!`;
 
-if (toCelsius() <= 100) {
-	alert('DAMN its cold outside!');
-} else {
-	alert('Its feelin suuuper nice out!');
-}
+// if (toCelsius() <= 100) {
+// 	alert('DAMN its cold outside!');
+// } else {
+// 	alert('Its feelin suuuper nice out!');
+// }
 
 toCelsius(200);
+
+//Exercising Higher Order Functions
+
+const consoles = ['SNES', 'PS', 'N64', 'PS5'];
+
+function conFun(arr, fn) {
+	for (let i = 0; i<arr.length; i++) {
+		fn(arr[i]);
+	}
+}
+
+function addCon(name) {
+	console.log("COOL! a " + name);
+}
+
+conFun(consoles, addCon);
