cf8a186039e5802819e07528e966e90952eb3f56
TaylorMade <warriorotk18@gmail.com>
2022-10-18 15:06:09 -0600

    created celcius to fahrenheit function

--- js/function.js
+++ js/function.js
@@ -4,3 +4,8 @@ let x = dadsFunc(18, 2);
 function dadsFunc(a, b) {
 		return a * b;
 }
+
+function toCelcius(fahrenheit) {
+	return (5/9) * (fahrenheit - 32);
+}
+let temp = `It's currently is ${toCelcius(122)} degrees outside!`;
