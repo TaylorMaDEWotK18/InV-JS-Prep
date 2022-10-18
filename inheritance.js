021b9c1ddc08521325ab521fa01f7294ca5e55ff
TaylorMade <warriorotk18@gmail.com>
2022-10-17 18:49:26 -0600

    Practicing inheritance and created new file

---
+++ inheritance.js
@@ -0,0 +1,16 @@
+// class Knight {
+// 	name() {
+// 		return "Galahad";
+// 	}
+// }
+
+// const me = new Knight();
+// const you = new Knight();
+
+class Person {
+	talk() {
+		return 'Talking';
+	}
+}
+
+const me = new Person();
