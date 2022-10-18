d036245c5489d852acfe7e279484ed21cdbf6b53
TaylorMade <warriorotk18@gmail.com>
2022-10-18 15:19:56 -0600

    created conditional but no sure why its not reading right

--- js/function.js
+++ js/function.js
@@ -5,7 +5,15 @@ function dadsFunc(a, b) {
 		return a * b;
 }
 
-function toCelcius(fahrenheit) {
+function toCelsius(fahrenheit) {
 	return (5/9) * (fahrenheit - 32);
 }
-let temp = `It's currently is ${toCelcius(122)} degrees outside!`;
+let temp = `It's currently is ${toCelsius(86)} degrees outside!`;
+
+if (toCelsius() <= 100) {
+	alert('DAMN its cold outside!');
+} else {
+	alert('Its feelin suuuper nice out!');
+}
+
+toCelsius(200);
