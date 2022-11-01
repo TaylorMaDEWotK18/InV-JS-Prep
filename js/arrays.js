118ec1f9319b6c03c524c7a08e1a2e052683f9f6
TaylorMade <warriorotk18@gmail.com>
2022-11-01 16:31:32 -0600

    practicing adding array elements inside literal

--- js/arrays.js
+++ js/arrays.js
@@ -1,2 +1,8 @@
 const games = ['MGS','God of War','Prodeus','RE']
 console.log(games[2]);
+console.log(games.length);
+games.push('Final Fantasy');
+games.unshift('Donkey Kong');
+games.pull('Donkey Kong');
+
+const main = document.querySelector('main');
