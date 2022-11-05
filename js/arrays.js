fa8d91c046e9ecc4b3b2a37aeea28909e73d4e15
TaylorMade <warriorotk18@gmail.com>
2022-11-05 13:31:32 -0600

    working with shift and pop

--- js/arrays.js
+++ js/arrays.js
@@ -4,3 +4,7 @@ console.log(games.length);
 games.push('Final Fantasy');
 games.unshift('Donkey Kong');
 
+const numbers = [100, 200, 300, 400, 500];
+
+let firstOut = numbers.shift();
+let lastIn = numbers.pop();
