ad1e7ab224e5f7cd19c10238dce03abff46df406
TaylorMade <warriorotk18@gmail.com>
2022-11-23 00:20:08 -0700

    exercised dot notation and declaring easy property in object

--- js/objects.js
+++ js/objects.js
@@ -1,5 +1,9 @@
 const belmont = {
 	firstName: "Trevor",
 	weapon: "whip",
-	secondaryW: "Cross Boomerang" 
+	secondaryW: ["Cross Boomerang", "Holy Water", "Dagger"],
+	age: "unknown",
 }
+belmont.clan = "Brotherhood of Light"
+
+console.log(belmont)
